44328779636c756c2bc6c72e4b5003e6ec938e0f
Luciano (IBM) <lucianocfernandes@gmail.com>
2025-06-17 22:14:09 -0400

    Fix code convention

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+    Emotion Detection API
+"""
 import json
 from flask import Flask, request
 from EmotionDetection import emotion_detector
@@ -6,11 +9,17 @@ app = Flask("Emotion Detector API")
 
 @app.route("/emotionDetector", methods=["GET"])
 def emotion_detect():
+    """
+        Emotion Detector endpoint
+    """
     text_to_analyse = request.args.get("textToAnalyse")
     result = json.loads(emotion_detector(text_to_analyse))
     dominant_emotion = result.pop("dominant_emotion")
     if dominant_emotion:
-        return f"For the given statement, the system response is {result}. The dominant emotion is {dominant_emotion}."
+        return f"""
+            For the given statement, the system response is {result}. 
+            The dominant emotion is {dominant_emotion}.
+        """
 
     return "Invalid text! Please try again!"
 
